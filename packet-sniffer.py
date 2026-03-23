65c987174b30d842f276476b9656c2fe3a96c460
Reabetswe <putukacaiphus@gmail.com>
2026-03-23 11:53:44 +0200

    Added logging functionality and improved packet anaylysis

--- packet-sniffer.py
+++ packet-sniffer.py
@@ -2,18 +2,30 @@ from scapy.all import sniff, IP, TCP, UDP, ICMP
 
 def process_packet(packet):
     if packet.haslayer(IP):
-        ip_layer = packet[IP]
-        print(f"\n[+] IP Packet: {ip_layer.src} -> {ip_layer.dst}")
+        ip = packet[IP]
+        src = ip.src
+        dst = ip.dst
+
+        log = f"[+] {src} -> {dst}"
 
         if packet.haslayer(TCP):
-            print("[+] Protocol: TCP")
+            tcp_layer = packet[TCP]
+            log += f" | TCP Port: {tcp.dport}"
+
+            if tcp.dport not in [80, 443]:
+                log += " | ALERT: Unusual Port"
 
         elif packet.haslayer(UDP):
-            print("[+] Protocol: UDP")
+            udp = packet[UDP]
+            log += f" | UDP Port: {udp.dport}"
 
         elif packet.haslayer(ICMP):
-            print("[+] Protocol: ICMP")
+            log += " | ICMP Traffic"
 
-print("Starting advanced packet sniffer...")
+        print(log)
 
-sniff(prn=process_packet, count=20)
+        with open("sniffer_log.txt", "a") as file: file.write(log + "\n")
+
+print("Starting packet sniffer with loggin...\n")
+
+sniff(prn=process_packet, count=30)
